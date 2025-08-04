6403ba9f952377cac3492f757ed05d3e546e5d90
el-noir <mudasirshah9777@gmail.com>
2025-08-04 06:27:37 +0500

    Create Sign In and resetPassword Schemas

---
+++ src/schemas/resetPasswordSchema.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod'
+
+export const forgotPasswordSchema = z.object({
+  email: z.string().email({ message: "Enter a valid email" }),
+});
+
+export const resetPasswordSchema = z.object({
+  token: z.string().min(10), // From email
+  newPassword: z.string().min(6, { message: "Password must be at least 6 characters" }),
+});

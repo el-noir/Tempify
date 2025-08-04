0dcb6b09c737a59a0da59c572c9e55fba54bd4cf
el-noir <mudasirshah9777@gmail.com>
2025-08-04 20:07:02 +0500

    Completed Code - store create

--- src/app/api/store/create/page.ts
+++ src/app/api/store/create/page.ts
@@ -5,6 +5,7 @@ import dbConnect from "@/lib/connection/dbConnect";
 import StoreModel from "@/model/Store";
 import StorePlanModel from "@/model/StorePlan";
 import {nanoid} from 'nanoid';
+import { success } from "zod";
 
 export async function POST(req:Request) {
     const session = await getServerSession(authOptions)
@@ -39,20 +40,53 @@ try {
     }
 
     // check if plan exist in db
+    const plan = await StorePlanModel.findById(planId)
+    
+    if(!plan) {
+             return NextResponse.json(
+        { success: false, message: "Invalid store plan selected" },
+        { status: 404 }
+      ) 
+    }
+
     // generate a slug
+        const slugBase = name.trim().toLowerCase().replace(/[^a-z0-9]+/g, "-")
+        const slug = `${slugBase}-${nanoid(6)}`
 
     // expire data decide
+        const expiresAt = new Date(
+            Date.now() + plan.durationHours * 60 * 60 * 1000
+        )
 
     // create store and save in db
+        const newStore = new StoreModel({
+            ownerId: session.user._id,
+            planId,
+            name,
+            slug,
+            description,
+            expiresAt
+        })
 
     // return response of success
+    return NextResponse.json({
+        success: true,
+        message: "Store created successfully",
+        store: {
+            _id: newStore._id,
+            name: newStore.name,
+            slug: newStore.slug,
+            expiresAt: newStore.expiresAt
+        },
+    }, {status: 201})
+
 } catch (error) {
-    
+        console.error("Store creation error:", error)
+    return NextResponse.json(
+      { success: false, message: "Failed to create store" },
+      { status: 500 }
+    )
 }
-
-
-    
-
 
 }
 
